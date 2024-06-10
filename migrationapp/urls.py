aa0749c4b3ba9fe42a23dd257be0fae1bdb3522f
Chandrusam0538 <153303848+Chandrusam0538@users.noreply.github.com>
2024-06-10 23:09:39 +0530

    Update urls.py

--- migrationapp/urls.py
+++ migrationapp/urls.py
@@ -1,33 +1,13 @@
-# # from django.urls import path
-
-
-# from django.contrib import admin
-# from django.urls import path
-# from migrationapp import views
-
-# urlpatterns = [ 
-#     path('', views.login, name='home'),  # Set login view as the home page
-#     path('Signup/', views.signup, name='SignUp'),
-#     path('login/', views.login, name='login'),
-#     path('board/', views.board, name='board'),
-#     # path('dashboard/', views.dashboard, name='dashboard'),
-#     path('Database_Connectivity/', views.dashboard, name='dashboard'),
-
-#     path('display_data/', views.data_catalogue, name='data_catalogue'),
-#     # path('scan_and_store_data/', views.scan_and_store_data, name='scan_and_store_data'),
-#     path('data_mig/', views. data_mig, name='data_mig'),
-#     # Add other necessary paths
-# ]
-
-
-
 from django.contrib import admin
 from django.urls import path
 from migrationapp import views
+from django.urls import path
+from django.views.generic import RedirectView
+from migrationapp import views
 
 urlpatterns = [ 
-    path('', views.login, name='home'),  # Set login view as the home page
-    path('signup/', views.new_user, name='new_user'),
+    path('', views.login, name='home'),  
+    path('Signup/', views.signup, name='SignUp'),
     path('login/', views.login, name='login'),
     path('board/', views.board, name='board'),
     # path('dashboard/', views.dashboard, name='dashboard'),
@@ -35,32 +15,7 @@ urlpatterns = [
 
     path('display_data/', views.data_catalogue, name='data_catalogue'),
     # path('scan_and_store_data/', views.scan_and_store_data, name='scan_and_store_data'),
-    path('data_mig/', views. data_mig, name='data_mig'),
-    # Add other necessary paths
-]
+    path('data_mig/', views. data_mig, name='data_mig')
+    # path('signup/', views.new_user, name='new_user'),
+]    
 
-
-
-#signup fuct
-# def new_user(request):
-#     if request.method == "POST":
-#         username = request.POST.get('username')
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-#         re_enter_password = request.POST.get('Re_enter_password')
-
-#         # Check if passwords match
-#         if password != re_enter_password:
-#             return HttpResponse("Passwords do not match. Please try again.")
-
-#         try:
-#             # Create the user
-#             user = User.objects.create_users(username=username, email=email, password=password)
-#             # Optionally, you can set additional fields
-#             user.save()
-#             return redirect('login')  # Redirect to login page after successful signup
-#         except Exception as e:
-#             logger.error(f"Failed to create user: {e}")
-#             return HttpResponse(f"Failed to create user. Please try again. Error: {e}")
-
-#     return render(request, 'migrationapp/signup.html')
