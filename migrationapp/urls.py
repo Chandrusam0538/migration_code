55cb97472aaec157619e44f4cfb1d4944e0294b5
Chandrusam0538 <153303848+Chandrusam0538@users.noreply.github.com>
2024-06-22 20:12:14 +0530

    Update urls.py

--- migrationapp/urls.py
+++ migrationapp/urls.py
@@ -7,7 +7,7 @@ from migrationapp import views
 
 urlpatterns = [ 
     path('', views.login, name='home'),  
-    path('Signup/', views.signup, name='SignUp'),
+    path('Signup/', views.signup, name='Signup'),
     path('login/', views.login, name='login'),
     path('board/', views.board, name='board'),
     # path('dashboard/', views.dashboard, name='dashboard'),
@@ -16,8 +16,12 @@ urlpatterns = [
     path('display_data/', views.data_catalogue, name='data_catalogue'),
     # path('scan_and_store_data/', views.scan_and_store_data, name='scan_and_store_data'),
     path('data_mig/', views. data_mig, name='data_mig'),
-    path('db/', views.present_db, name='present_db')
+    path('db/', views. present_db, name='present_db'),
     # path('signup/', views.new_user, name='new_user'),
-    
-    path('server_user_view/', views.server_users_view, name='server_users_view')
+    path('server_user_view/', views.server_users_view, name='server_users_view'),
+
+    path('databases/', views.database_display, name='database_display'),
+    path('databases/<str:object_database>/', views.retrieve_metadata, name='retrieve_metadata'),
+
 ]    
+
